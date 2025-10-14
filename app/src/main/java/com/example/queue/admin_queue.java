d03f27af8d330de4564dd4313bbadca2536469ff
AbigiaAS <asa7793@mavs.uta.edu>
2025-10-13 23:51:45 -0500

    Admin queue

--- app/src/main/java/com/example/queue/admin_queue.java
+++ app/src/main/java/com/example/queue/admin_queue.java
@@ -1,1 +1,14 @@
+package com.example.queue;
 
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class admin_queue extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.amin_queue); // uses existing typo XML
+    }
+}
